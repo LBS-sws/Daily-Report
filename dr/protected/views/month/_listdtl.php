6b2e459a37c1f977b3c9421d63925b9f5e85b1b9
anxiaxia <1286571499@qq.com>
2018-10-26 14:58:24 +0800

    月报表图标错误

--- dr/protected/views/month/_listdtl.php
+++ dr/protected/views/month/_listdtl.php
@@ -1,5 +1,5 @@
 <tr class='clickable-row' data-href='<?php echo $this->getLink('A09', 'month/view', 'month/view', array('index'=>$this->record['id'],'city'=>$this->record['cityname']));?>'>
-	<td><?php echo $this->drawEditButton('A09', 'monthly/edit', 'monthly/view', array('index'=>$this->record['id'])); ?></td>
+	<td><?php echo $this->drawEditButton('A09', 'month/view', 'month/view', array('index'=>$this->record['id'],'city'=>$this->record['cityname']));?></td>
     <td><?php echo $this->record['city']; ?></td>
     <td><?php echo $this->record['year_no']; ?></td>
 	<td><?php echo $this->record['month_no']; ?></td>
