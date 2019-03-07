7a5870d3beddf7af32986fdeed68f8e11f2abed6
anxiaxia <1286571499@qq.com>
2019-03-07 10:28:03 +0800

    月报计算公式修改

--- dr/protected/config/system.php
+++ dr/protected/config/system.php
@@ -6,16 +6,16 @@ return array(
 		'name'=>'Daily Report',
 		'icon'=>'fa fa-pencil-square-o',
 	),
-	'acct'=>array(
-		'webroot'=>'http://192.168.3.102/ac-uat',
-		'name'=>'Accounting',
-		'icon'=>'fa fa-money',
-	),
-	'ops'=>array(
-		'webroot'=>'http://192.168.3.102/op-uat',
-		'name'=>'Operation',
-		'icon'=>'fa fa-gears',
-	),
+//	'acct'=>array(
+//		'webroot'=>'http://192.168.3.102/ac-uat',
+//		'name'=>'Accounting',
+//		'icon'=>'fa fa-money',
+//	),
+//	'ops'=>array(
+//		'webroot'=>'http://192.168.3.102/op-uat',
+//		'name'=>'Operation',
+//		'icon'=>'fa fa-gears',
+//	),
 	'hr'=>array(
 		'webroot'=>'http://192.168.3.102/hr-uat',
 		'name'=>'Personnel',
@@ -26,27 +26,27 @@ return array(
 		'name'=>'Sales',
 		'icon'=>'fa fa-suitcase',
 	),
-	'quiz'=>array(
-		'webroot'=>'http://192.168.3.102/qz-uat',
-		'name'=>'Quiz',
-		'icon'=>'fa fa-pencil',
-	),
-	'sp'=>array(
-		'webroot'=>'http://192.168.3.102/sp-uat',
-		'name'=>'Academic Credit',
-		'icon'=>'fa fa-cube',
-	),
-	'onlib'=>array(
-		'webroot'=>'https://onlib.lbsapps.com/seeddms',
-		'script'=>'remoteLoginOnlib',
-		'name'=>'Online Library',
-		'icon'=>'fa fa-book',
-		'external'=>array(
-				'layout'=>'onlib',
-				'update'=>'saveOnlib',		//function defined in UserFormEx.php
-				'fields'=>'fieldsOnlib',
-			),
-	),
+//	'quiz'=>array(
+//		'webroot'=>'http://192.168.3.102/qz-uat',
+//		'name'=>'Quiz',
+//		'icon'=>'fa fa-pencil',
+//	),
+//	'sp'=>array(
+//		'webroot'=>'http://192.168.3.102/sp-uat',
+//		'name'=>'Academic Credit',
+//		'icon'=>'fa fa-cube',
+//	),
+//	'onlib'=>array(
+//		'webroot'=>'https://onlib.lbsapps.com/seeddms',
+//		'script'=>'remoteLoginOnlib',
+//		'name'=>'Online Library',
+//		'icon'=>'fa fa-book',
+//		'external'=>array(
+//				'layout'=>'onlib',
+//				'update'=>'saveOnlib',		//function defined in UserFormEx.php
+//				'fields'=>'fieldsOnlib',
+//			),
+//	),
 /*
 	'apps'=>array(
 		'webroot'=>'https://app.lbsgroup.com.tw/web',
