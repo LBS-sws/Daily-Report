aadeb8e1dc30b2a862679d43bdabed8d69632187
anxiaxia <1286571499@qq.com>
2020-07-27 14:28:36 +0800

    客户服务栏位修正

--- dr/protected/models/ServiceForm.php
+++ dr/protected/models/ServiceForm.php
@@ -265,7 +265,7 @@ class ServiceForm extends CFormModel
             $sql = "delete from swo_service_contract_no where service_id=".$this->id;
             $connection->createCommand($sql)->execute();
         }else{
-            $sql = "update  swo_service_contract_no set contract_no = '$this->contract_no'  where service_id=".$this->id;
+            $sql = "insert into swo_service_contract_no(contract_no,service_id,status_dt,status) values('$this->contract_no','$this->id','$this->status_dt','$this->status') on duplicate key update contract_no='$this->contract_no', status_dt='$this->status_dt', status='$this->status'";
             $connection->createCommand($sql)->execute();
         }
     }
