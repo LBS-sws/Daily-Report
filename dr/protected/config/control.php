a281d16262568e4e7a26b1a27912e37fb1019af8
anxiaxia <1286571499@qq.com>
2020-10-15 13:56:36 +0800

    分析电邮权限

--- dr/protected/config/control.php
+++ dr/protected/config/control.php
@@ -4,4 +4,7 @@ return array(
 	'Complaint Read All'=>array('access'=>'CN01',),
 	'QC Read All'=>array('access'=>'CN02',),
     'QC Delete'=>array('access'=>'CN03',),
+    'Integrated data email (region)'=>array('access'=>'CN012',),
+    'Integrated data email (regional)'=>array('access'=>'CN013',),
+    'Integrated data email (all)'=>array('access'=>'CN014',),
 );
