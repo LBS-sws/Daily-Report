338e9ad0480922fbc95f6cb8c73d2cc3baa032d2
shenchao <shenchao@lbsgroup.com.cn>
2021-06-17 13:57:39 +0800

    swo_service表的royalty和royaltys字段精確到三個小數點

---
+++ dr/protected/data/2021-06-17(service表的royalty字段精確到三個小數點).sql
@@ -0,0 +1,2 @@
+alter table swo_service modify royalty decimal(6,3) UNSIGNED ZEROFILL NULL DEFAULT 000.000;
+alter table swo_service modify royaltys decimal(4,3) UNSIGNED ZEROFILL NULL DEFAULT 0.000;
