1cdc8629c78619da4c956714a3aaedc78812f1c4
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2021-06-30 17:33:30 +0800

    暂停及终止客户邮件发送-原因sql

---
+++ dr/protected/data/swo_service_end_reasons.sql
@@ -0,0 +1,28 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : ldb
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : swoper
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2021-06-30 17:31:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `swo_service_end_reasons`
+-- ----------------------------
+DROP TABLE IF EXISTS `swo_service_end_reasons`;
+CREATE TABLE `swo_service_end_reasons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `reason` varchar(1000) DEFAULT NULL COMMENT '原因',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+
