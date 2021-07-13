57de5ae5903c681ea13e665e35afeaf169e4724d
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2021-07-13 14:37:11 +0800

    优化客户服务新增及修改限制--取消修改

--- dr/protected/controllers/ServiceController.php
+++ dr/protected/controllers/ServiceController.php
@@ -66,20 +66,20 @@ class ServiceController extends Controller
 			$model = new ServiceForm($_POST['ServiceForm']['scenario']);
 			$model->attributes = $_POST['ServiceForm'];
 			//新增限制修改日期
-            $xianzhi = false;// || ($_POST['ServiceForm']['scenario']!='new' && date('Y-m',strtotime($_POST['ServiceForm']['status_dt']))<=date('Y-m',strtotime("-2 month")))
-            if ($_POST['ServiceForm']['status_desc']=='新增' && date('Y-m',strtotime($_POST['ServiceForm']['first_dt']))<=date('Y-m',strtotime("-2 month"))){
-                $xianzhi = true;
-                if ($_POST['ServiceForm']['scenario']=='new'){
-                    $message ="首次日期不能早于".date('Y-m-01',strtotime("-1 month"));
-                }else{
-                    $message ="该月资料不允许修改，以免影响过往积分数据导致激励点变动";
-                }
-            }
-			if ($xianzhi){
-
-                Dialog::message(Yii::t('dialog','Validation Message'), $message);
-                $this->render('form',array('model'=>$model,));
-            }else{
+//            $xianzhi = false;// || ($_POST['ServiceForm']['scenario']!='new' && date('Y-m',strtotime($_POST['ServiceForm']['status_dt']))<=date('Y-m',strtotime("-2 month")))
+//            if ($_POST['ServiceForm']['status_desc']=='新增' && date('Y-m',strtotime($_POST['ServiceForm']['first_dt']))<=date('Y-m',strtotime("-2 month"))){
+//                $xianzhi = true;
+//                if ($_POST['ServiceForm']['scenario']=='new'){
+//                    $message ="首次日期不能早于".date('Y-m-01',strtotime("-1 month"));
+//                }else{
+//                    $message ="该月资料不允许修改，以免影响过往积分数据导致激励点变动";
+//                }
+//            }
+//			if ($xianzhi){
+//
+//                Dialog::message(Yii::t('dialog','Validation Message'), $message);
+//                $this->render('form',array('model'=>$model,));
+//            }else{}
                 if ($model->validate()) {
                     $model->saveData();
 //				$model->scenario = 'edit';
@@ -91,8 +91,6 @@ class ServiceController extends Controller
                     $this->render('form',array('model'=>$model,));
                 }
             }
-
-		}
 	}
 
 	public function actionView($index)
