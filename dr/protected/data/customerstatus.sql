898fd69fffd22c71cc62058a03d2e10b1c2c7dcf
lbs-percylee <percylee@lbsgroup.com.hk>
2022-10-10 12:14:36 +0800

    《优化》史伟莎客户查询 - 不計算其他客户 / 產品買賣

---
+++ dr/protected/data/customerstatus.sql
@@ -0,0 +1,20 @@
+DELIMITER //
+DROP FUNCTION IF EXISTS CustomerStatus //
+CREATE FUNCTION `CustomerStatus`(p_id int unsigned, p_code varchar(20), p_name varchar(1000), p_city char(5)) RETURNS char(1)
+BEGIN
+DECLARE status char(1);
+SET status = (
+SELECT case when count(a.id)=0 then 'U'
+when sum(case when a.status<>'T' then 1 else 0 end) > 0 then 'A'
+else 'T'
+end
+FROM swo_service a
+LEFT OUTER JOIN swo_service b ON substring(a.company_name, 1, char_length(p_code))=substring(b.company_name, 1, char_length(p_code))
+AND a.status_dt < b.status_dt AND a.cust_type=b.cust_type
+WHERE b.id IS NULL AND a.city=p_city
+AND (a.company_id=p_id OR substring(a.company_name, 1, char_length(p_code))=p_code)
+AND a.cust_type not in (7,9)
+);
+RETURN status;
+END //
+DELIMITER ;
