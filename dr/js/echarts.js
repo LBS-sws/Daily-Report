8346ee207569cd24d91c94f2fab666dcf711e192
xiangsong <wait973@qq.com>
2022-12-13 09:29:46 +0800

    Vsersion 1.0

--- dr/js/echarts.js
+++ dr/js/echarts.js
@@ -16,6 +16,7 @@
 * KIND, either express or implied.  See the License for the
 * specific language governing permissions and limitations   
 * under the License.
+*  Vsersion 1.0
 */
 
 (function (global, factory) {
