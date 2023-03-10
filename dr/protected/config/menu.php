c6c24d4b76238ce24d9cfa66967f58dd12b1a7ac
shenchao <shenchao@lbsgroup.com.cn>
2023-03-10 17:17:46 +0800

    菜單欄的位置調整

--- dr/protected/config/menu.php
+++ dr/protected/config/menu.php
@@ -187,13 +187,13 @@ return array(
                 'access'=>'G03',
                 'url'=>'/summary/index',
             ),
-            'Summary Set'=>array(
-                'access'=>'G04',
-                'url'=>'/summarySet/index',
-            ),
             'Comparison'=>array(
                 'access'=>'G05',
                 'url'=>'/comparison/index',
+            ),
+            'Summary Set'=>array(
+                'access'=>'G04',
+                'url'=>'/summarySet/index',
             ),
             'Comparison Set'=>array(
                 'access'=>'G06',
