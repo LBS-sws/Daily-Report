c85b9dab8d2e479b200aa06da4978f1fb0243dad
shenchao <shenchao@lbsgroup.com.cn>
2023-10-05 14:44:29 +0800

    销售生产力分析的页内搜索

--- dr/protected/views/salesAnalysis/form.php
+++ dr/protected/views/salesAnalysis/form.php
@@ -13,11 +13,12 @@ $this->pageTitle=Yii::app()->name . ' - SalesAnalysis Form';
     .click-tr>.fa:before{ content: "\f062";}
     .click-tr.show-tr>.fa:before{ content: "\f063";}
     .table-fixed{ table-layout: fixed;}
-    .radio-inline,select{ opacity: 0.6;pointer-events: none;}
     .form-group{ margin-bottom: 0px;}
     .table-fixed>thead>tr>th,.table-fixed>tfoot>tr>td,.table-fixed>tbody>tr>td{ text-align: center;vertical-align: middle;font-size: 12px;border-color: #333;}
     .table-fixed>tfoot>tr>td,.table-fixed>tbody>tr>td{ text-align: right;}
     .table-fixed>thead>tr>th.header-width{ height: 0px;padding: 0px;overflow: hidden;border-width: 0px;line-height: 0px;}
+
+    tr.searchTr{ background:#b8e9fb;}
 </style>
 
 <section class="content-header">
@@ -51,13 +52,25 @@ $this->pageTitle=Yii::app()->name . ' - SalesAnalysis Form';
         <div id="yw0" class="tabbable">
             <div class="box-info" >
                 <div class="box-body" >
-                    <div class="col-lg-12" style="padding-bottom: 15px;">
+                    <div class="col-lg-6" style="padding-bottom: 15px;">
                         <div class="form-group">
-                            <?php echo $form->labelEx($model,'search_date',array('class'=>"col-sm-2 control-label")); ?>
-                            <div class="col-sm-2">
+                            <?php echo $form->labelEx($model,'search_date',array('class'=>"col-sm-4 control-label")); ?>
+                            <div class="col-sm-5">
                                 <?php echo $form->textField($model, 'search_date',
                                     array('readonly'=>true,'prepend'=>"<span class='fa fa-calendar'></span>")
                                 ); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6" style="padding-bottom: 15px;">
+                        <div class="form-group">
+                            <?php
+                            echo TbHtml::label("页内搜索",'',array('class'=>"col-sm-4 control-label"));
+                            ?>
+                            <div class="col-sm-5">
+                                <?php
+                                echo  TbHtml::dropDownList("searchEx",'',array(""=>"-- 全部 --"),array('id'=>'searchEx'));
+                                ?>
                             </div>
                         </div>
                     </div>
@@ -189,6 +202,65 @@ $js="
                 }
             }
         });
+    });
+    
+    var arr={};
+    $('#salesAnalysis').find('tbody>tr').not('.tr-end').each(function(){
+        var dept_name = $(this).children('td').eq(1).text();
+        dept_name = dept_name.trim();
+        if(dept_name!=''){
+            arr[dept_name]=0;
+        }
+    });
+    $.each(arr,function(key,val){
+        $('#searchEx').append('<option value=\"'+key+'\">'+key+'</option>');
+    });
+    
+    $('#searchEx').change(function(){
+        var searchText = $(this).val();
+        if(searchText==''){
+            $('#salesAnalysis').find('tbody>tr').show();
+            $('#salesAnalysis').find('.searchTr').remove();
+        }else{
+            $('#salesAnalysis .click-tr').each(function(){
+                var trClick=$(this);
+                var list=[];
+                trClick.prevAll('tr').each(function(){
+                    if($(this).hasClass('tr-end')){
+                        return false;
+                    }else{
+                        var dept_name=$(this).find('td').eq(1).text();
+                        dept_name=dept_name.trim();
+                        if(dept_name==searchText){
+                            $(this).show();
+                            list.push($(this));
+                        }else{
+                            $(this).hide();
+                        }
+                    }
+                });
+                $(this).next('.searchTr').remove();
+                if(list.length!=0){
+                    var countList=[];
+                    var html ='<tr class=\"searchTr\">';
+                    html+='<td colspan=\"3\">搜索合计</td><td>-</td><td>-</td><td>-</td>';
+                    $.each(list,function(i,objTr){
+                        objTr.find('td').slice(6).each(function(key,objTd){
+                            objTdText=$(objTd).text();
+                            if(countList[key]==undefined){
+                                countList[key]=0;
+                            }
+                            countList[key]+=objTdText==''||isNaN(objTdText)?0:parseFloat(objTdText);
+                        });
+                    });
+                    $.each(countList,function(j,value){
+                        html+='<td>'+value+'</td>';
+                    });
+                    html+='</tr>';
+                    $(this).after(html);
+                }
+            });
+        }
     });
 ";
 Yii::app()->clientScript->registerScript('calcFunction',$js,CClientScript::POS_READY);
