dca9380e52732b0f7a80c873d2773955bac7233a
anlondon@foxmail.com <gitlabpassword>
2023-11-23 20:57:11 +0800

    feat: 为保安账号添加 派单系统 切换控制

---
+++ nu/protected/config/menu.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'New United'=>array(
+		'access'=>'NU00',
+		'icon'=>'fa-pencil-square-o',
+		'items'=>array(
+		    //派单系统
+            'System Change'=>array(
+                'access'=>'NU00',
+                'url'=>'/nu',
+            ),
+		),
+	)
+);
