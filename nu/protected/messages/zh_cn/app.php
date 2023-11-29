4c76ae41bab800de9858ce3e50128ae5f6d059d7
anlondon@foxmail.com <gitlabpassword>
2023-11-29 10:05:24 +0800

    feat: 派单系统融入LBS系统配置

---
+++ nu/protected/messages/zh_cn/app.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'System Change'=>'系统切换',
+    'New United'=>'派单系统',
+);
+?>
---
+++ nu/protected/messages/zh_tw/app.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'System Change'=>'系統切換',
+    'New United'=>'派單系統',
+);
+?>
