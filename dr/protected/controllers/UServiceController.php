366ccffd6c72a9544d93085695ad4785bf766434
shenchao <shenchao@lbsgroup.com.cn>
2024-05-22 15:10:02 +0800

    修复技术员生产力分析查询问题

--- dr/protected/controllers/UServiceController.php
+++ dr/protected/controllers/UServiceController.php
@@ -68,7 +68,7 @@ class UServiceController extends Controller
             }
             $i = ceil($model->search_month/3);//向上取整
             $model->search_quarter = 3*$i-2;
-            $model->city = Yii::app()->user->city();
+            $model->city = null;
             $model->condition = 1;
             $model->seniority_min = 3;
         }
