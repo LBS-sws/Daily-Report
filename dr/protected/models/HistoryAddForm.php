60ad0d0dbf129d79d5abf1982cf3a05cfb72f6ef
shenchao <shenchao@lbsgroup.com.cn>
2024-09-05 13:42:46 +0800

    修改管理层的历史新增表格（3）

--- dr/protected/models/HistoryAddForm.php
+++ dr/protected/models/HistoryAddForm.php
@@ -25,6 +25,7 @@ class HistoryAddForm extends CFormModel
 
 	public $th_sum=1;//所有th的个数
 
+    public $downJsonText='';
 	/**
 	 * Declares customized attribute labels.
 	 * If not declared here, an attribute would have a label that is
@@ -148,6 +149,8 @@ class HistoryAddForm extends CFormModel
                 }
                 if($str==="U"&&key_exists($dateStr."_u",$data)){
                     $data[$dateStr."_u"]+=$value;
+                }
+                if($str==="U"&&key_exists($dateStr."_OTHER",$data)){
                     $data[$dateStr."_OTHER"]+=$value;
                 }
             }
@@ -367,12 +370,15 @@ class HistoryAddForm extends CFormModel
     public function tableBodyHtml(){
         $html="";
         if(!empty($this->data)){
+            $this->downJsonText=array();
             $html.="<tbody>";
             $moData = key_exists("MO",$this->data)?$this->data["MO"]:array();
             unset($this->data["MO"]);//澳门需要单独处理
             $html.=$this->showServiceHtml($this->data);
             $html.=$this->showServiceHtmlForMO($moData);
             $html.="</tbody>";
+            $this->downJsonText=json_encode($this->downJsonText);
+            $html.=TbHtml::hiddenField("excel",$this->downJsonText);
         }
         return $html;
     }
@@ -417,8 +423,9 @@ class HistoryAddForm extends CFormModel
                 foreach ($bodyKey as $keyStr){
                     $text = key_exists($keyStr,$cityList)?$cityList[$keyStr]:"0";
                     $tdClass = HistoryAddForm::getTextColorForKeyStr($text,$keyStr);
-                    $inputHide = TbHtml::hiddenField("excel[MO][]",$text);
-                    $html.="<td class='{$tdClass}'><span>{$text}</span>{$inputHide}</td>";
+                    //$inputHide = TbHtml::hiddenField("excel[MO][]",$text);
+                    $this->downJsonText["excel"]['MO'][$keyStr]=$text;
+                    $html.="<td class='{$tdClass}'><span>{$text}</span></td>";
                 }
                 $html.="</tr>";
             }
@@ -464,9 +471,11 @@ class HistoryAddForm extends CFormModel
                                 $allRow[$keyStr]+=is_numeric($text)?floatval($text):0;
                             }
                             $tdClass = HistoryAddForm::getTextColorForKeyStr($text,$keyStr);
-                            $inputHide = TbHtml::hiddenField("excel[{$regionList['region']}][list][{$cityList['city']}][]",$text);
 
-                            $html.="<td class='{$tdClass}'><span>{$text}</span>{$inputHide}</td>";
+                            $this->downJsonText["excel"][$regionList['region']]['list'][$cityList['city']][$keyStr]=$text;
+                            //$inputHide = TbHtml::hiddenField("excel[{$regionList['region']}][list][{$cityList['city']}][]",$text);
+
+                            $html.="<td class='{$tdClass}'><span>{$text}</span></td>";
                         }
                         $html.="</tr>";
                     }
@@ -493,8 +502,9 @@ class HistoryAddForm extends CFormModel
         foreach ($bodyKey as $keyStr){
             $text = key_exists($keyStr,$data)?$data[$keyStr]:"0";
             $tdClass = HistoryAddForm::getTextColorForKeyStr($text,$keyStr);
-            $inputHide = TbHtml::hiddenField("excel[{$data['region']}][count][]",$text);
-            $html.="<td class='{$tdClass}' style='font-weight: bold'><span>{$text}</span>{$inputHide}</td>";
+            //$inputHide = TbHtml::hiddenField("excel[{$data['region']}][count][]",$text);
+            $this->downJsonText["excel"][$data['region']]['count'][$keyStr]=$text;
+            $html.="<td class='{$tdClass}' style='font-weight: bold'><span>{$text}</span></td>";
         }
         $html.="</tr>";
         return $html;
@@ -509,6 +519,10 @@ class HistoryAddForm extends CFormModel
 
     //下載
     public function downExcel($excelData){
+        if(!is_array($excelData)){
+            $excelData = json_decode($excelData,true);
+            $excelData = key_exists("excel",$excelData)?$excelData["excel"]:array();
+        }
         $this->validateDate("","");
         $headList = $this->getTopArr();
         $excel = new DownSummary();
